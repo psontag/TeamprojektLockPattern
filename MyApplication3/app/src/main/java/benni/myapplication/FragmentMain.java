a5956cbd1422af42567ff8caf81d258c70ed6d82
bennnil <bennnil-github@yahoo.com>
2014-07-18 00:40:40 +0200

    NFC Pass als Fragment
    Das Schreiben ist nach wie vor nicht möglich.

---
+++ MyApplication3/app/src/main/java/benni/myapplication/FragmentMain.java
@@ -0,0 +1,252 @@
+package benni.myapplication;
+
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+
+public class FragmentMain extends Fragment {
+
+    NfcAdapter adapter;
+    PendingIntent pendingIntent;
+    IntentFilter writeTagFilters[];
+    boolean writeMode;
+    Tag mytag;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		return inflater.inflate(R.layout.fragment_my, container, false);
+	}
+	
+    @Override
+    public void onStart() {
+        super.onStart();
+        Button btnWrite = (Button) getView().findViewById(R.id.write_button);
+        Button btnCheck = (Button) getView().findViewById(R.id.Check_button);
+        final TextView password_w = (TextView)getView().findViewById(R.id.password_write);
+        final TextView password_c = (TextView)getView().findViewById(R.id.password_check);
+        btnWrite.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if(mytag==null){
+                        Alert("There is no Tag to write on.");
+                    }else {
+                        write(password_w.getText().toString(), mytag);
+                    }
+                } catch (IOException e) {
+                        Alert("Error! Was the Tag close enough?");
+                        e.printStackTrace();
+                    } catch (FormatException e) {
+                        Alert("Error! Was the Tag close enough?");
+                        e.printStackTrace();
+                }
+            }
+        });
+
+        btnCheck.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if(mytag==null){
+                        Alert("There is no Tag to read from.");
+                    }else {
+                        String pwtag = null;
+                        pwtag = read(mytag);
+                        Alert(pwtag);
+                        if(pwtag.equals(password_c.getText().toString())){
+                            Alert("Matching password!");
+                        }
+                        else {
+                            Alert("The passwords do not match!");
+                        }
+                    }
+                } catch (IOException e) {
+                    Alert("Error! Was the Tag close enough?");
+                    e.printStackTrace();
+                } catch (FormatException e) {
+                    Alert("Error! Was the Tag close enough?");
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
+
+        adapter = NfcAdapter.getDefaultAdapter(getActivity());
+        pendingIntent = PendingIntent.getActivity(getActivity(), 0, new Intent(getActivity(), getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+        writeTagFilters = new IntentFilter[] { tagDetected };
+
+    }
+
+
+
+    private void write(String text, Tag tag) throws IOException, FormatException {
+
+        NdefRecord[] records = { createRecord(text) };
+        NdefMessage message = new NdefMessage(records);
+        // Get an instance of Ndef for the tag.
+        Ndef ndef = Ndef.get(tag);
+        // Enable I/O
+        ndef.connect();
+        // Write the message
+        ndef.writeNdefMessage(message);
+        // Close the connection
+        ndef.close();
+        Alert("Done");
+
+    }
+
+    private String read(Tag tag) throws IOException, FormatException {
+        String password = null;
+        // Get an instance of Ndef for the tag.
+        Ndef ndef = Ndef.get(tag);
+        // Enable I/O
+        ndef.connect();
+        // Read the message
+        NdefMessage ndefMessage = ndef.getCachedNdefMessage();
+
+        NdefRecord[] records = ndefMessage.getRecords();
+        for (NdefRecord ndefRecord : records) {
+            if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
+                try {
+                    password =  readText(ndefRecord);
+                } catch (UnsupportedEncodingException e) {
+                }
+            }
+        }
+        // Close the connection
+        ndef.close();
+        return password;
+
+    }
+
+    private String readText(NdefRecord record) throws UnsupportedEncodingException {
+        /*
+         * See NFC forum specification for "Text Record Type Definition" at 3.2.1
+         *
+         * http://www.nfc-forum.org/specs/
+         *
+         * bit_7 defines encoding
+         * bit_6 reserved for future use, must be 0
+         * bit_5..0 length of IANA language code
+         */
+
+        byte[] payload = record.getPayload();
+
+        // Get the Text Encoding
+        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+
+        // Get the Language Code
+        int languageCodeLength = payload[0] & 0063;
+
+        // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+        // e.g. "en"
+
+        // Get the Text
+        return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+    }
+
+
+
+
+    private NdefRecord createRecord(String text) throws UnsupportedEncodingException {
+        String lang       = "en";
+        byte[] textBytes  = text.getBytes();
+        byte[] langBytes  = lang.getBytes("US-ASCII");
+        int    langLength = langBytes.length;
+        int    textLength = textBytes.length;
+        byte[] payload    = new byte[1 + langLength + textLength];
+
+        // set status byte (see NDEF spec for actual bits)
+        payload[0] = (byte) langLength;
+
+        // copy langbytes and textbytes into payload
+        System.arraycopy(langBytes, 0, payload, 1,              langLength);
+        System.arraycopy(textBytes, 0, payload, 1 + langLength, textLength);
+
+        NdefRecord recordNFC = new NdefRecord(NdefRecord.TNF_WELL_KNOWN,  NdefRecord.RTD_TEXT,  new byte[0], payload);
+
+        return recordNFC;
+    }
+
+
+    protected void onNewIntent(Intent intent) {
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+            mytag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            Alert("Found Tag");
+        }
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        WriteModeOff();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        WriteModeOn();
+    }
+
+    private void WriteModeOn(){
+        writeMode = true;
+        adapter.enableForegroundDispatch(getActivity(), pendingIntent, writeTagFilters, null);
+    }
+
+    private void WriteModeOff(){
+        writeMode = false;
+        adapter.disableForegroundDispatch(getActivity());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void Alert(String message){
+        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+        alert.setTitle("Information");
+        alert.setMessage(message);
+        alert.setPositiveButton("Okay",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+        alert.show();
+    }
+}
